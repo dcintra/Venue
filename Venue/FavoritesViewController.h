c5b50afba0c2d9a0062b6255d6185dc8f69ea9a3
dcintra <daniel@lcintra.com>
2014-03-18 13:58:58 -0400

    Added Favorites Controller

---
+++ Venue/FavoritesViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoritesViewController.h
+//  Venue
+//
+//  Created by Daniel Cintra on 3/18/14.
+//  Copyright (c) 2014 Daniel Cintra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoritesViewController : UIViewController
+
+@end
